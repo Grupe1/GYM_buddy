dea9ef713e1de219683173b72a20e8ab7aa01d57
Juozas Balčikonis <juobal3@ktu.lt>
2025-03-01 02:26:38 +0200

    Initial commit

---
+++ gymBuddy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace gymBuddy
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ gymBuddy/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace gymBuddy
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnStartClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Welcome!", "Let's set up your gym schedule!", "OK");
+        }
+    }
+
+}
